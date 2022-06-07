584fd7def7a113e00b3368b57cd829d63b6b7e16
nwpgrifon <nwpgrifon@gmail.com>
2022-06-07 06:05:32 +0300

    fix feign version

--- src/main/java/ru/alfabank/client/GiphyClient.java
+++ src/main/java/ru/alfabank/client/GiphyClient.java
@@ -1,6 +1,6 @@
 package ru.alfabank.client;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.alfabank.dto.GiphyResponse;
--- src/main/java/ru/alfabank/client/OXRClient.java
+++ src/main/java/ru/alfabank/client/OXRClient.java
@@ -1,7 +1,7 @@
 package ru.alfabank.client;
 
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
